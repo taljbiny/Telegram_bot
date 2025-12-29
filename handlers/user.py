739eedd14071a557e9c48766c870540a47748ad4
taljbiny <taljbinin@gmail.com>
2025-12-29 22:12:53 +0300

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -1,1 +1,27 @@
+from telebot import types
+from database import get_connection
 
+def user_handlers(bot):
+
+    @bot.message_handler(commands=['start'])
+    def start(message):
+        conn = get_connection()
+        cur = conn.cursor()
+
+        cur.execute(
+            "INSERT OR IGNORE INTO users (telegram_id, username) VALUES (?, ?)",
+            (message.from_user.id, message.from_user.username)
+        )
+
+        conn.commit()
+        conn.close()
+
+        kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        kb.add("➕ إيداع", "➖ سحب")
+        kb.add("💰 رصيدي")
+
+        bot.send_message(
+            message.chat.id,
+            "أهلاً بك 👋\nاختر من القائمة:",
+            reply_markup=kb
+        )
