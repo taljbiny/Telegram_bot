2dad26c424934060434488861a7c76383dff5382
taljbiny <taljbinin@gmail.com>
2025-12-30 00:45:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,14 +5,13 @@ import threading
 from config import TOKEN, ADMINS
 from database import init_db, get_connection
 
-from handlers.admin_panel import admin_handlers
 from handlers.user import user_handlers
+from handlers.admin_panel import admin_handlers
 from handlers.support import support_handlers
 
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
-# Flask endpoint للبقاء مستيقظ
 @app.route("/")
 def home():
     return "Bot is alive"
@@ -23,7 +22,7 @@ def run_flask():
 # تهيئة قاعدة البيانات
 init_db()
 
-# إضافة Handlers
+# إضافة جميع Handlers
 user_handlers(bot)
 admin_handlers(bot)
 support_handlers(bot)
