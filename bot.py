64ff70c30d6d86cd98b258c7e1fde85977d71ac9
taljbiny <taljbinin@gmail.com>
2025-12-29 21:38:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,27 @@
+import telebot
+from flask import Flask
+import threading
 
+from config import TOKEN
+from database import init_db
+from handlers.user import user_handlers
+from handlers.admin_panel import admin_handlers
+
+bot = telebot.TeleBot(TOKEN)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is alive"
+
+def run_flask():
+    app.run(host="0.0.0.0", port=8080)
+
+init_db()
+user_handlers(bot)
+admin_handlers(bot)
+
+threading.Thread(target=run_flask).start()
+
+print("Bot is running...")
+bot.infinity_polling()
