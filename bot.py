83a19d38aa1f473f56edb9dc56c7992c7efd5043
taljbiny <taljbinin@gmail.com>
2025-12-30 10:52:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,11 @@
 import telebot
-from telebot import types
-from flask import Flask
-import threading
-from config import TOKEN, ADMINS
-from database import init_db
-from handlers.user import user_handlers
-from handlers.admin_panel import admin_handlers
-from handlers.support import support_handlers
+from config import TOKEN
+from handlers import user, support, admin_panel
 
 bot = telebot.TeleBot(TOKEN)
-app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Bot is alive"
+user.register(bot)
+support.register(bot)
+admin_panel.register(bot)
 
-def run_flask():
-    app.run(host="0.0.0.0", port=8080)
-
-# تهيئة قاعدة البيانات
-init_db()
-
-# إضافة جميع Handlers
-user_handlers(bot)
-admin_handlers(bot)
-support_handlers(bot)
-
-# تشغيل Flask في Thread منفصل
-threading.Thread(target=run_flask).start()
-
-print("Bot is running...")
 bot.infinity_polling()
